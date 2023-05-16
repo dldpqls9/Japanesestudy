47d0ce99409ec62b23d87ed16a46985d795c022a
dldpq <dldpqls9@gmail.com>
2023-05-16 17:35:07 +0900

    first

---
+++ app/src/main/java/com/cookandroid/myjapanesestudy/Newspage.java
@@ -0,0 +1,14 @@
+package com.cookandroid.myjapanesestudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Newspage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newspage);
+    }
+}
---
+++ app/src/main/java/com/cookandroid/myjapanesestudy/Quizpage.java
@@ -0,0 +1,14 @@
+package com.cookandroid.myjapanesestudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Quizpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizpage);
+    }
+}
